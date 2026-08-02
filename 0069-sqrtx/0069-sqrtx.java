416b5fe4abba00d6994aecaf45e88e5ea58d1f63
Tejan Sharma <144901470+Tejan3737@users.noreply.github.com>
2026-08-03 03:08:38 +0530

    Time: 1 ms (97.56%), Space: 42.8 MB (23.81%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int mySqrt(int x) {
+        int start = 0;
+        int end = x ; 
+        int ans=-1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(mid*mid==x){
+                return mid;
+            }else if((long)mid*mid>(long)x){
+                end=mid-1;
+            }else if(mid*mid<x){
+                ans=mid;
+                start=mid+1;
+            }
+        }
+        return ans;
+    }
+}
