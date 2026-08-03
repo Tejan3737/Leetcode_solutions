430ae4e74f29377ec49dbaf448570dda8459a32f
Tejan Sharma <144901470+Tejan3737@users.noreply.github.com>
2026-08-03 23:34:08 +0530

    Time: 8 ms (97.27%), Space: 48.6 MB (90.84%) - LeetHub

--- 0324-wiggle-sort-ii/0324-wiggle-sort-ii.java
+++ 0324-wiggle-sort-ii/0324-wiggle-sort-ii.java
@@ -1,14 +1,12 @@
 class Solution {
     public void wiggleSort(int[] nums) {
         Arrays.sort(nums);
+        int[] ans = nums.clone();
         int n = nums.length;
-        int[] ans=nums.clone();
-        int index=0;
-        int left=(n-1)/2;
-        int right=n-1;
+        int left = (n-1)/2;
+        int right = n-1;
         for(int i = 0 ; i<n ; i++){
-            nums[i] = (i%2==0)?ans[left--]:ans[right--];
+            nums[i]=(i%2==0)?ans[left--]:ans[right--];
         }
     }
-
 }
