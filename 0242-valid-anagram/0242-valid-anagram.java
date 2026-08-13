72b3b4e7c37fb3d0354fb2a2816d6c97953fa8f2
Tejan Sharma <144901470+Tejan3737@users.noreply.github.com>
2026-08-13 09:41:45 +0530

    Time: 1 ms (99.82%), Space: 44.4 MB (66.96%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] arr1 = new int[26];
+        int[] arr2 = new int[26];
+        for(char ch : s.toCharArray()){
+            arr1[(int)ch-'a']++;
+        }
+        for(char ch : t.toCharArray()){
+            arr2[(int)ch-'a']++;
+        }
+        for(int i = 0 ; i<26 ; i++){
+            if(arr1[i]!=arr2[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
