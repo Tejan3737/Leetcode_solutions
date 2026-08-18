7e2a9c6b6087a6dc103c8ce91d180fc643bb4b50
Tejan Sharma <144901470+Tejan3737@users.noreply.github.com>
2026-08-18 09:41:13 +0530

    Time: 7 ms (99.99%), Space: 82.8 MB (29.65%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
@@ -1,40 +1,64 @@
 class Solution {
-    public int count(int[] bloomDay , int day , int flowers){
-        int count = 0;
-        int subcount = 0;
-        for(int bd : bloomDay){
-            if(bd<=day){
-                subcount++;
-                if(subcount==flowers){
-                    subcount=0;
-                    count++;
+    
+        
+    boolean possible(int[] bloomDay, int m, int k, int mid)
+    {
+        int flowers=0;
+        int bouquets=0;
+        int n=bloomDay.length;
+        for(int i=0; i<n; i++)
+        {
+            if(bloomDay[i] <= mid)
+            {
+                flowers++;
+                if(flowers == k)
+                {
+                    bouquets++;
+                    flowers = 0;
                 }
-            }else{
-                subcount=0;
             }
+            else
+            {
+                flowers = 0;
+                if ((m - bouquets) * k > n - i - 1)
+                    break;
+            }
+            if (bouquets >= m)
+                break;
         }
-        return count;
+
+        return bouquets >= m;
+
     }
     public int minDays(int[] bloomDay, int m, int k) {
-        if(bloomDay.length<m*k){
+        int n=bloomDay.length;
+        if ((long) m * k > n)
             return -1;
-        }
-        int start = 0 ;
-        int end = bloomDay[0];
-        int ans = -1;
-        for(int i = 0 ; i < bloomDay.length ; i++){
-            end=Math.max(bloomDay[i],end);
-        }
-        while(start<=end){
-            int mid = start + (end - start)/2;
-            int bouquet = count(bloomDay,mid , k );
-            if(bouquet>=m){
-                end = mid - 1;
-                ans = mid;
-            }else if(bouquet<m){
-                start = mid+1;
+        
+        int r = 0;
+        for (int i=0; i < n; i++) {
+            if (bloomDay[i] > r) {
+                r = bloomDay[i];
             }
         }
-        return ans;
+
+        int l=1;
+        int result = -1;
+        while(l <= r)
+        {
+            int mid = l + (r - l)/2;
+            if(possible(bloomDay, m, k, mid))
+            {
+                result = mid;
+                r = mid  - 1;
+            }
+
+            else
+            {
+                l = mid + 1;
+            }
+        }
+        
+        return result;
     }
 }
